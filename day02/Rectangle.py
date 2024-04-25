9d3cc78e77d0caa9350c23c497b64fc479d7f3ac
Maher Salhab <maher@salhab.com>
2024-04-25 13:59:30 +0300

    update

---
+++ day02/Rectangle.py
@@ -0,0 +1,5 @@
+length = input("Lenght: ")   
+width = input("Width: ")
+area = int(length) * int(width) 
+print(f"The area is {area}")
+
---
+++ day02/what_is_this_language.py
@@ -0,0 +1,9 @@
+language = input("what is this programming language?")
+if language == "python": 
+    print("you are write!")
+    print("still here")
+else:
+    print("not good")
+
+print("Done")
+
