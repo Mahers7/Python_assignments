58565ede7ede908ab7bc280a5a4d4fc787f89233
Maher Salhab <maher@salhab.com>
2024-05-30 13:14:32 +0300

    add ignore

--- day02/add_numbers.py
+++ day02/add_numbers.py
@@ -2,8 +2,8 @@
 # width = input("Width: ")
 first = input("first number: ")
 second = input("second number: ")
-#sum = first + second
-#print(f"The sum is {sum}")    #to convert them to numbers we need to add int()
+# sum = first + second
+# print(f"The sum is {sum}")    #to convert them to numbers we need to add int()
 
 sum = int(first) + int(second)
 print(f"The sum is {sum}")
